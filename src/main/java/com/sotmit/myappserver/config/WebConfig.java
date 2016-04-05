5c3c9d14ce99c1835c0f931d7a2f8055e7e48df7
netcat <alexandr.teh@gmail.com>
2016-04-05 10:22:49 +0300

    simple  Servlet

---
+++ src/main/java/com/sotmit/myappserver/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.sotmit.myappserver.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.sotmit.myappserver")
+public class WebConfig extends WebMvcConfigurerAdapter{
+}
